caa52b3e4e51c109d4dcccc61feaba961b27cb6a
SameeranB <sameeranbandishti93@gmail.com>
2019-04-17 19:13:29 +0530

    Fixed Directories

--- ecomm/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/sameeran/Documents/Projects/Ecommerce_django/ecomm/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
